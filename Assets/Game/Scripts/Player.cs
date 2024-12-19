08f61a11ca8b0f9c0c7778576733db963d7a21a1
GyulovGames <155297083+GyulovGames@users.noreply.github.com>
2024-12-19 19:32:38 +0300

    Проблема решена. Скоро обновление...

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -12,56 +12,140 @@ public class Player : MonoBehaviour
 
     private Camera gameCamera;
     private EventSystem eventSystem;
+    private FloatingJoystick joystick;
 
     private bool sounds = true;
-    private bool cursorIsVisible = false;
     private bool isGamePaused = false;
-
+    private bool cursorIsVisible = false;
+    private bool isDesctop = true;
+    private float desiredAngle;
 
     private void Start()
     {
-        Cursor.visible = false;
-        sounds = YG2.saves.soundsSettings;
+        FindAndGetCamera();
+        SubscribeToEvents();
+        SetSoundsSettings();
+        FindAndGetJoystick();
+        DetermineDeviceType();
+        FindAndGetEventSystem();
+    }
 
+    private void FindAndGetCamera()
+    {
         GameObject cam = GameObject.FindGameObjectWithTag("GameCamera");
         gameCamera = cam.GetComponent<Camera>();
-
-        GameObject even = GameObject.FindGameObjectWithTag("EventSystem");
-        eventSystem = even.GetComponent<EventSystem>();
-
+    }
+    private void SubscribeToEvents()
+    {
         GameCanvas.PauseEvent.AddListener(GamePause);
         GameCanvas.ResumeGame.AddListener(ResumeGame);
     }
+    private void SetSoundsSettings()
+    {
+        sounds = YG2.saves.soundsSettings;
+    }
+    private void FindAndGetJoystick()
+    {
+        GameObject joystickObject = GameObject.FindGameObjectWithTag("Joystick");
+        joystick = joystickObject.GetComponent<FloatingJoystick>();
+    }
+    private void DetermineDeviceType()
+    {
+        string deviceType = YG2.envir.deviceType;
+
+        if (deviceType == "desktop")
+        {
+            isDesctop = true;
+            aimTransform.gameObject.SetActive(true);
+            joystick.gameObject.SetActive(false);
+        }
+        else if (deviceType == "mobile" || deviceType == "tablet")
+        {
+            isDesctop = false;
+            aimTransform.gameObject.SetActive(false);
+            joystick.gameObject.SetActive(true);
+        }
+        else
+        {
+            isDesctop = false;
+            aimTransform.gameObject.SetActive(false);
+            joystick.gameObject.SetActive(true);
+        }
+    }
+    private void FindAndGetEventSystem()
+    {
+        GameObject even = GameObject.FindGameObjectWithTag("EventSystem");
+        eventSystem = even.GetComponent<EventSystem>();
+    }
+
+
 
     private void Update()
     {
         if (!isGamePaused)
         {
-            CusorAndAimVisible();
-
-            if (!eventSystem.IsPointerOverGameObject() && Input.GetMouseButton(0))
+            if (isDesctop)
             {
-                assaultRifle.Shoot();
+                
+                DesctopControll();
             }
-
-
-            Vector3 cursorScreenPoint = Input.mousePosition;
-            Ray ray = gameCamera.ScreenPointToRay(cursorScreenPoint);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            else if(!isDesctop)
             {
-                Vector3 cursorWorldPosition = hit.point;
-                cursorWorldPosition.z = -0.55f;
-                aimTransform.position = cursorWorldPosition;
+                
 
-                Vector3 direction = aimTransform.position - transform.position;
-                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0, 0, angle);
-            }
+                MobileControll();
+            }           
         }
     }
 
+    private void DesctopControll()
+    {
+        CusorAndAimVisible();
+
+        Vector3 cursorScreenPoint = Input.mousePosition;
+        Ray ray = gameCamera.ScreenPointToRay(cursorScreenPoint);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 cursorWorldPosition = hit.point;
+            cursorWorldPosition.z = -0.55f;
+            aimTransform.position = cursorWorldPosition;
+
+            Vector3 direction = aimTransform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        if (!eventSystem.IsPointerOverGameObject() && Input.GetMouseButton(0))
+        {
+            assaultRifle.Shoot();
+        }
+    }
+    private void MobileControll()
+    {
+        if (Input.touchCount > 0)
+        {
+            Vector3 joystickVectrs = new Vector3(joystick.Horizontal, joystick.Vertical, 0);
+            Vector3 direction = joystickVectrs - Vector3.zero;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
+            if(joystick.Vertical != 0 || joystick.Horizontal != 0)
+            {
+                desiredAngle = angle;
+            }
+
+            if (joystickVectrs.x != 0 || joystickVectrs.y != 0)
+            {
+                assaultRifle.Shoot();
+            }
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, desiredAngle);
+        }
+    }
     private void CusorAndAimVisible()
     {
         if(eventSystem.IsPointerOverGameObject() && !cursorIsVisible)
